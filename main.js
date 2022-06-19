08525cb94b039004cb01e2b959deef601730cb1b
meee-low <my_milo_m@hotmail.com>
2022-06-19 09:20:47 -0300

    changed order of functions, renamed variables, added comments
    No changes to functionality.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let isRunning = false;
 
+// Internal functions.
+
 function addParticipant(name){
+    // This function creates a participant with the given name
+    // and adds it to the page as a button.
     if (name == ""){
         return;
     }
@@ -8,23 +12,25 @@ function addParticipant(name){
     let btn = document.createElement("button");
     btn.classList.add("button");
     btn.classList.add("participant");
-    btn.dataset.secondsSpokenTotal = 0;
-    btn.dataset.secondsSpokenNow = 0;
-    // add a name
+    btn.dataset.secondsSpokenTotal = 0; // seconds spoken total
+    btn.dataset.secondsSpokenNow = 0; //seconds spoken in the current session
+
+    // add a name div inside the button (works as a label)
     let button_name = document.createElement("div");
     button_name.classList.add("name");
     button_name.innerHTML = name;
     btn.appendChild(button_name);
-    // initialize total time to 0
+
+    // initialize total time to 0 as a div inside the button
     let time = document.createElement("div");
     time.classList.add("time");
     time.innerHTML = "0:00";
     btn.appendChild(time);
 
-    //btn.addEventListener("click", "changeActive(this);");
+    // make button interactive
     btn.onclick = function() {trackerPressed(this);};
 
-    // make the button appear
+    // make the button appear in the correct area
     let trackers_area = document.getElementById("trackersArea");
     trackers_area.appendChild(btn);
     
@@ -32,95 +38,115 @@ function addParticipant(name){
     let name_field = document.getElementById("nameField");
     name_field.value = "";
 }
-
-function trackerPressed(btn){
-    if (Object.values(btn.classList).includes("activeButton")){
-        pause();
-    }
-    else{
-        changeActive(btn);
-    }    
+function run(){
+    // runs the timer that updates every 1000 miliseconds (1s).
+    setInterval(updateTime, 1000);
 }
 
-function changeActive(newActiveBtn){
-    isRunning = true;
-
-    // disable old button
-    let oldActiveBtn = document.getElementsByClassName("activeButton")[0];
-    if (oldActiveBtn != undefined){
-        oldActiveBtn.classList.remove("activeButton");
-        oldActiveBtn.dataset.secondsSpokenNow = 0;
-    }
+function updateTime(){
+    // Ticks every second and updates the time internally.
+    // This is triggered by the setInterval function inside run().
+    // Visual changes are handled by updateLabels().
     
-    // enable new button
-    newActiveBtn.classList.add("activeButton");
-
-    //update Labels
-    updateLabels();
-}
-
-function update(){
-    //deals with the internal values, then calls updateLabels().
-    
-    //ignore if not running
+    // Ignore if not running.
     if (!isRunning){
         return;
     }
-    // update times every second
+
+    // ticks up times for the active button every second
     let activeButton = document.getElementsByClassName("activeButton")[0];
     if (activeButton != undefined){
         activeButton.dataset.secondsSpokenTotal++;
         activeButton.dataset.secondsSpokenNow++;
     }
-    updateLabels();
+
+    updateLabels(); // refresh labels
 }
 
 function updateLabels(){
-    //deals with the external/visible values.
-    let buttons = document.getElementsByClassName("participant");
+    // Deals with the external/visible labels.
+    let trackerButtons = document.getElementsByClassName("participant");
     
-    //loop through buttons and fix them 
-    for (const btn of buttons){
-        let timeSpokenTotal = secondsToMMSS(btn.dataset.secondsSpokenTotal);
-        let timeSpokenNow = secondsToMMSS(btn.dataset.secondsSpokenNow);
-        let timeToDisplay;
-        if (Object.values(btn.classList).includes("activeButton")){
+    // Loop through every button and fix the labels.
+    for (const trackerBtn of trackerButtons){
+        let timeSpokenTotal = secondsToMMSS(trackerBtn.dataset.secondsSpokenTotal);
+        let timeSpokenNow = secondsToMMSS(trackerBtn.dataset.secondsSpokenNow);
+        let timeToDisplay; // the label that eventually gets displayed
+        if (Object.values(trackerBtn.classList).includes("activeButton")){
+            // if the button is active, also show the time spent in the current turn
             timeToDisplay = timeSpokenTotal + " (Current: " + timeSpokenNow + ")";
         }
         else{
+            // if the button is not active, only show the total time spent
             timeToDisplay = timeSpokenTotal;
         }
-        btn.getElementsByClassName("time")[0].innerHTML = timeToDisplay;
+        // finally, change the label
+        trackerBtn.getElementsByClassName("time")[0].innerHTML = timeToDisplay;
     };
+}
+
+// Functions that directly deal with input/interaction.
+
+function trackerPressed(trackerBtn){
+    // Triggers when you press a button.
+    // If the button is already active, just pause. Otherwise, change active button.
+    if (Object.values(trackerBtn.classList).includes("activeButton")){
+        // button is active, so pause
+        pause();
+    }
+    else{
+        // make this the new active button
+        changeActiveTracker(trackerBtn);
+    }    
+}
+
+function changeActiveTracker(newActiveTracker){
+    isRunning = true; // starts running if not already
+    DisableActiveTracker(); //disable old active tracker
+    newActiveTracker.classList.add("activeButton"); // enable new tracker
+    updateLabels();
+}
+
+function DisableActiveTracker(){
+    // find and disable currently active button
+    let currentlyActiveTracker = document.getElementsByClassName("activeButton")[0]; //find
+    if (currentlyActiveTracker != undefined){
+        // as long as you found something
+        currentlyActiveTracker.classList.remove("activeButton"); // disable
+        currentlyActiveTracker.dataset.secondsSpokenNow = 0; // reset time
+    }
 }
 
 function pause(){
     isRunning = false;
-
-    // disable old button
-    let oldActiveBtn = document.getElementsByClassName("activeButton")[0];
-    if (oldActiveBtn != undefined){
-        oldActiveBtn.classList.remove("activeButton");
-        oldActiveBtn.dataset.secondsSpokenNow = 0;
-    }
+    DisableActiveTracker();
     updateLabels();
 }
 
 function checkEnter(ele){
+    // Triggers when you press enter in the input box.
     if (event.key === "Enter"){
+        // send the text inside the input box to the addParticipant function
         addParticipant(ele.value);
     }
 }
 
 function startNew(){
     //remove all buttons and reset time
+    // Not implemented.
 }
 
+//Utilities, string formatting, etc.
+
 function secondsToMMSS(seconds){
-    let return_str = "";
+    // Takes in a number of seconds and returns a string in M:SS format.
+    // If more than 1 hour, returns H:MM:SS format.
+    
+    let return_str = ""; // gets added to as we go
     
     let mm = Math.floor(seconds / 60);
     if (mm >= 60){
+        // more than 1 hour
         let hh = Math.floor(mm / 60);
         return_str = hh + ":"
         mm = mm % 60;
@@ -139,6 +165,8 @@ function secondsToMMSS(seconds){
 }
 
 function padZeroes(num, len){
+    // add zeroes to the front of a number to make it a certain length
+    // returns a string, not a number
     let str = num.toString();
     while (str.length < len){
         str = "0" + str;
@@ -146,6 +174,4 @@ function padZeroes(num, len){
     return str;
 }
 
-function run(){
-    setInterval(update, 1000);
-}
+
